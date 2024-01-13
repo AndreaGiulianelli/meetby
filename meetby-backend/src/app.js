08281a25879c9e8dfddcbade8232f577a78e5329
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-13 11:22:21 +0100

    chore: setup mongoose

--- meetby-backend/src/app.js
+++ meetby-backend/src/app.js
@@ -1,11 +1,20 @@
 require('dotenv').config()
 
+const mongoose = require('mongoose')
 const express = require('express')
 
-const app = express()
-const httpServer = require('http').createServer(app)
 
-const serverPort = process.env.WEB_SERVER_PORT || 8080
-httpServer.listen(serverPort, () => {
-    console.log(`Web server is running on port ${serverPort}`)
+async function main() {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
+    const app = express()
+    const httpServer = require('http').createServer(app)
+
+    const serverPort = process.env.WEB_SERVER_PORT || 8080
+    httpServer.listen(serverPort, () => {
+        console.log(`Web server is running on port ${serverPort}`)
+    })
+}
+
+main().catch(error => {
+    console.log(`An error occurred: ${error}`)
 })
