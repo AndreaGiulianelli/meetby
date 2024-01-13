7008b18044dc5b4ceaff5926ca226bd9591b5934
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-13 11:06:29 +0100

    chore: configure base web server

--- meetby-backend/src/app.js
+++ meetby-backend/src/app.js
@@ -1,1 +1,11 @@
 require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+const httpServer = require('http').createServer(app)
+
+const serverPort = process.env.WEB_SERVER_PORT || 8080
+httpServer.listen(serverPort, () => {
+    console.log(`Web server is running on port ${serverPort}`)
+})
