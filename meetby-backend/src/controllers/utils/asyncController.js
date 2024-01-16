ccab6adcf322c8625cbdb2e6e155af598d5009f3
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-16 09:57:12 +0100

    chore: add logging when error happen

--- meetby-backend/src/controllers/utils/asyncController.js
+++ meetby-backend/src/controllers/utils/asyncController.js
@@ -1,5 +1,8 @@
 exports.asyncController = function(controller) {
     return (req, res) => {
-        controller(req, res).catch(() => { return res.status(500).send() })
+        controller(req, res).catch((err) => { 
+            console.log(err)
+            return res.status(500).send() 
+        })
     }
 }
