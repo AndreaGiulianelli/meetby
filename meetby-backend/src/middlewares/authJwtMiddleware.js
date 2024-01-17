f4ebc1fb594978d4ad9154427dc986674fb98f7c
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-17 10:41:49 +0100

    chore: add middleware for auth or guest requests

--- meetby-backend/src/middlewares/authJwtMiddleware.js
+++ meetby-backend/src/middlewares/authJwtMiddleware.js
@@ -1,10 +1,6 @@
 const jwtService = require("../services/jwtService")
 
-exports.verify = (req, res, next) => {
-    const token = req.headers.authorization
-    if (!token) {
-        return res.status(401).send({ message: "No auth token provided" })
-    }
+function verifyToken(token, req, res, next) {
     jwtService
         .verifyToken(token)
         .then(result => {
@@ -18,4 +14,25 @@ exports.verify = (req, res, next) => {
         .catch(_ => {
             return res.status(500).send()
         })
+}
+
+exports.verify = (req, res, next) => {
+    const token = req.headers.authorization
+    if (!token) {
+        return res.status(401).send({ message: "No auth token provided" })
+    }
+    verifyToken(token, req, res, next)
+}
+
+exports.verifyOrGuest = (req, res, next) => {
+    const token = req.headers.authorization
+    if (token) {
+        verifyToken(token, req, res, next)
+    } else {
+        if(!req.query.guest) {
+            return res.status(401).json({ message: "No auth token and no guest data is provided" })
+        }
+        req.guestEmail = req.query.guest
+        next()
+    }
 }
