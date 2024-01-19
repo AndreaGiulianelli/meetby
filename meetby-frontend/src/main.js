8a68e202bd56215c4aa4ac6c7a353a64be1ddf43
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-19 12:37:02 +0100

    chore: add configuration for vuetify icons

--- meetby-frontend/src/main.js
+++ meetby-frontend/src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -13,6 +15,13 @@ import router from './router'
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi,
+        },
+      },
 })
 const pinia = createPinia()
 const app = createApp(App)
