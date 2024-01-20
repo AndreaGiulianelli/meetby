add272d8f6749318af3fbd7ac0f476fc5d09df9b
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-20 19:08:36 +0100

    chore: include homepage in the routes

--- meetby-frontend/src/router/index.js
+++ meetby-frontend/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomePage from '@/views/HomePage.vue'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
