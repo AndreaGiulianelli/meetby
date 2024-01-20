2957c1d9429e4cb16c6c3074430df638569d47b2
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-20 15:17:19 +0100

    feat: create footer

---
+++ meetby-frontend/src/components/SimpleFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-footer class="align-end flex-grow-0 bg-paletteWhite">
+        <div class="text-center w-100"> 
+            {{ new Date().getFullYear() }} — <strong>meetby</strong>
+        </div>
+    </v-footer>
+</template>
