e473a80654c21bd76796191560afdbf5ca2d333c
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-21 17:39:36 +0100

    chore: add user services

---
+++ meetby-frontend/src/services/UserService.js
@@ -0,0 +1,16 @@
+import httpClient from './http/httpClient'
+
+class UserService {
+    async getUsersStarting(startWith) {
+        const response = await httpClient.get("/users", {
+            params: { startsWith: startWith }
+        })
+        if (response.status === 200) {
+            return response.data
+        } else {
+            return []
+        }
+    }
+}
+
+export default new UserService()
