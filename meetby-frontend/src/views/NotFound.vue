4105dd3cb151cfd2ec2d0584fcf7a9d517f1e294
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-21 18:13:52 +0100

    feat: create not found page

---
+++ meetby-frontend/src/views/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="px-4">Ops... this page was not found within <i>*meetby*</i></h1>
+    <p class="px-4 mt-10 font-italic">Don't worry, everyone makes a wrong turn...</p>
+</template>
