a25ff3c5bd8ed0966a67523a89f514651672262e
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-21 14:55:21 +0100

    feat: add signup page

--- meetby-frontend/src/views/HomePage.vue
+++ meetby-frontend/src/views/HomePage.vue
@@ -12,7 +12,7 @@
     <v-row class="mt-16">
         <v-spacer></v-spacer>
         <v-col cols="10" md="4" lg="3">
-            <v-btn block class="black-btn" aria-label="Join and plan">
+            <v-btn block class="black-btn" aria-label="Join and plan" to="/signup">
                 Join and plan
             </v-btn>
             <p class="font-weight-bold">Already have an account? <router-link to="/login" class="text-paletteBlue">Login</router-link></p>
---
+++ meetby-frontend/src/views/SignUpPage.vue
@@ -0,0 +1,113 @@
+<script setup>
+import { ref } from 'vue'
+
+const isFormValid = ref(false)
+
+const name = ref('')
+const surname = ref('')
+const username = ref('')
+const email = ref('')
+const password = ref('')
+
+const showPassword = ref(false)
+const showConfirmPassword = ref(false)
+
+const nameRules = [
+    value => {
+        if (value) return true
+        return 'Name is required.'
+    }
+]
+
+const surnameRules = [
+    value => {
+        if (value) return true
+        return 'Surname is required.'
+    }
+]
+
+const usernameRules = [
+    value => {
+        if (value) return true
+        return 'Username is required.'
+    },
+    // TODO: implement username already exists
+]
+
+const passwordRules = [
+    value => {
+        if (value) return true
+        return 'Password is required.'
+    },
+    value => {
+        if (value.length >=8) return true
+        return 'Min 8 characters'
+    }
+]
+
+const confirmPasswordRules = [
+    value => {
+        if (value === password.value) return true
+        return 'The two password are different.'
+    }
+]
+
+const emailRules = [
+    value => {
+        if (value) return true
+        return 'E-mail is requred.'
+    },
+    value => {
+        if (/.+@.+\..+/.test(value)) return true
+        return 'E-mail must be valid.'
+    },
+]
+
+
+function signup() {
+    if (isFormValid.value) {
+        // TODO: implement
+    }
+}
+
+</script>
+
+<template>
+    <v-row>
+        <v-col cols="12" class="mb-2">
+            <div class="px-6 pt-5 text-center">
+                <p class="main-title font-weight-black mb-2">Create your meetby account</p>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-spacer></v-spacer>
+        <v-col cols="11" md="4" lg="3">
+            <v-form v-model="isFormValid" @submit.prevent="signup">
+                <v-container>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field class="mb-3" hide-details="auto" :rules="nameRules" bg-color="paletteGrey" v-model="name" label="Name" color="black" type="text" autocomplete="true" required/>
+                            <v-text-field class="mb-3" hide-details="auto" :rules="surnameRules" bg-color="paletteGrey" v-model="surname" label="Surname" color="black" type="text" autocomplete="true" required/>
+                            <v-text-field class="mb-3" hide-details="auto" :rules="usernameRules" bg-color="paletteGrey" v-model="username" label="Username" color="black" type="text" required/>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field class="mb-3" hide-details="auto" :rules="emailRules" bg-color="paletteGrey" v-model="email" label="Email" color="black" type="email" autocomplete="true" required/>
+                            <v-text-field class="mb-3" hide-details="auto" :rules="passwordRules" hint="At least 8 characters" bg-color="paletteGrey" v-model="password" label="Password" 
+                                :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" :type="showPassword ? 'text' : 'password'" color="black" @click:append-inner="showPassword = !showPassword" required />
+                            <v-text-field class="mb-3" hide-details="auto" :rules="confirmPasswordRules" bg-color="paletteGrey" label="Confirm password" :append-inner-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                                :type="showConfirmPassword ? 'text' : 'password'" color="black" @click:append-inner="showConfirmPassword = !showConfirmPassword" required />
+                        </v-col>
+                        <v-col cols="12" class="mt-3">
+                            <v-btn block type="submit" class="black-btn" aria-label="Signup">
+                                Sign Up
+                            </v-btn>
+                            <p class="font-weight-bold">Already have an account? <router-link to="/login" class="text-paletteBlue">Login</router-link></p>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-form>
+        </v-col>
+        <v-spacer></v-spacer>
+    </v-row>
+</template>
