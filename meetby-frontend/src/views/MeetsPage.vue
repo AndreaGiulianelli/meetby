e856d69ec543060d40331db8b28d94aa42b26544
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-22 11:39:59 +0100

    feat: add meets page

---
+++ meetby-frontend/src/views/MeetsPage.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref, watchEffect, } from 'vue'
+import FilteredSearchBar from '@/components/ui/FilteredSearchBar.vue'
+import MeetItem from '@/components/meet/MeetItem.vue'
+import MeetsService from '@/services/MeetsService.js'
+
+const tab = ref('')
+const meets = ref([])
+const search = ref('')
+
+watchEffect(() => {
+    setMeets(search.value.filter, (meet) => meet.title.toLowerCase().includes(search.value.search.toLowerCase()))
+})
+
+setMeets()
+
+function setMeets(status, filter) {
+    MeetsService.getAll(status).then(response => { meets.value = response.filter((value) => filter ? filter(value) : true) })
+}
+</script>
+
+<template>
+    <v-row class="h-100">
+        <v-spacer></v-spacer>
+        <v-col cols="12" md="6" lg="4">
+            <v-tabs v-model="tab" align-tabs="center">
+                <v-tab value="meets" :class="{ 'font-weight-bold' : tab == 'meets' }" class="text-subtitle-1">meets</v-tab>
+                <v-tab value="calendar" :class="{ 'font-weight-bold' : tab == 'calendar' }" class="text-subtitle-1">calendar</v-tab>
+            </v-tabs>
+
+            <v-window v-model="tab" class="h-100">
+                <v-window-item value="meets" class="mi h-100 pt-5">
+                    <FilteredSearchBar v-model="search" class="pb-0"/>
+
+                    <MeetItem 
+                        v-for="meet in meets"
+                        :key="meet._id"
+                        :id="meet._id"
+                        :title="meet.title"
+                        :creator="meet.meetCreator.name + ' ' + meet.meetCreator.surname"
+                        :partecipants="meet.invitedUsers.length + meet.invitedGuests.length + 1"
+                        :status="meet.plannedDateTime ? 'Planned' : 'Planning'"
+                    />
+
+                    <v-btn class="fab" icon="mdi-plus" size="large" color="paletteBlack" elevation="2" />
+                </v-window-item>
+                <v-window-item value="calendar" class="pt-5">
+                    Calendar
+                </v-window-item>
+            </v-window>
+        </v-col>
+        <v-spacer></v-spacer>
+    </v-row>
+</template>
+
+<style scoped>
+.fab {
+    position: fixed;   
+    bottom: 56px;
+    right: 16px;
+}
+</style>
