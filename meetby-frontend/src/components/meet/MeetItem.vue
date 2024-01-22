7036d85e154a534450425c408bce1fe582d872ee
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-22 13:37:35 +0100

    chore: make meet item editable

--- meetby-frontend/src/components/meet/MeetItem.vue
+++ meetby-frontend/src/components/meet/MeetItem.vue
@@ -4,7 +4,8 @@ const props = defineProps({
     title: String,
     creator: String,
     partecipants: Number,
-    status: String
+    status: String,
+    editable: Boolean,
 })
 </script>
 
@@ -12,25 +13,31 @@ const props = defineProps({
     <v-container>
         <v-row>
             <v-col cols="12" class="pt-0 pb-2">
-                <v-card color="paletteBlue" elevation="4" link @click.prevent="console.log(id)">
-                    <v-card-title class="pb-0">{{ props.title }}</v-card-title>
-                    <v-card-subtitle>by {{ props.creator }}</v-card-subtitle>
+                <v-card
+                    color="paletteBlue"
+                    :title="props.title"
+                    :subtitle="`by ${props.creator}`"
+                    elevation="4"
+                    to="/"
+                >
+                    <template v-slot:append>
+                        <p 
+                            class="mb-3 px-2 py-1 text-caption bg-paletteWhite rounded-lg font-weight-bold"
+                            :class="props.status == 'Planning' ? 'text-paletteOrange' : 'text-paletteGreen'"
+                        >{{ props.status }} ●</p>
+                    </template>
 
                     <v-card-text class="pt-2">
                         <v-icon size="small" icon="mdi-account-group"></v-icon>
                         <span class="text-caption font-weight-light">
                             {{ ` ${props.partecipants} ${props.partecipants === 1 ? 'partecipant' : 'partecipants'}` }}
                         </span>
-
-                        <v-row>
-                            <v-col cols="12" class="d-flex flex-row-reverse">
-                                <span 
-                                    class="px-2 py-1 text-caption bg-paletteWhite rounded-lg font-weight-bold"
-                                    :class="props.status == 'Planning' ? 'text-paletteOrange' : 'text-paletteGreen'"
-                                >{{ props.status }} ●</span>
-                            </v-col>
-                        </v-row>
                     </v-card-text>
+
+                    <v-card-actions v-if="editable">
+                        <v-spacer></v-spacer>
+                        <v-btn append-icon="mdi-pencil" to="/">Edit</v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
