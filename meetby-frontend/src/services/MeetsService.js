400bd7df2273a528f144a546fbca8173608a6a61
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-23 12:00:35 +0100

    chore: add get update availabilities and leave meeting service

--- meetby-frontend/src/services/MeetsService.js
+++ meetby-frontend/src/services/MeetsService.js
@@ -13,6 +13,30 @@ class MeetsService {
             return []
         }
     }
+
+    async get(meetId) {
+        const response = await httpClient.get(`/meets/${meetId}`)
+        if (response.status === 200) {
+            return response.data
+        } else {
+            return null
+        }
+    }
+
+    async update(meetId, updateBody) {
+        const response = await httpClient.put(`/meets/${meetId}`, updateBody)
+        return response.status === 204
+    }
+
+    async setPersonalAvailabilities(meetId, availabilities) {
+        const response = await httpClient.put(`/meets/${meetId}/availabilities`, availabilities)
+        return response.status === 204
+    }
+
+    async leaveMeeting(meetId) {
+        const response = await httpClient.delete(`/meets/${meetId}/partecipations`)
+        return response.status === 204
+    }
 }
 
 export default new MeetsService()
