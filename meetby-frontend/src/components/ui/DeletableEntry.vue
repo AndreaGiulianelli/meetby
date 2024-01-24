bc7dfe65beb5f764e6a6331817cca2f27c76c99d
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-24 14:13:55 +0100

    chore: add deletable entry component

---
+++ meetby-frontend/src/components/ui/DeletableEntry.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+    text: String,
+})
+const emit = defineEmits(['deleted'])
+
+function deleted() {
+    emit('deleted')
+}
+</script>
+
+<template>
+    <v-card
+        color="paletteGrey"
+        elevation="1"
+        class="text-caption"
+    >
+        <template v-slot:title>
+            <span class="text-button">{{ text }}</span>
+        </template>
+
+        <template v-slot:append>
+            <v-icon 
+                aria-label="Delete"
+                aria-hidden="false"
+                icon="mdi-close"
+                color="paletteBlack"
+                @click.prevent="deleted"
+            ></v-icon>
+        </template>
+    </v-card>
+</template>
