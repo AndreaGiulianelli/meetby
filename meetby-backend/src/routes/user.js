eb304bd600307e73110a047730cf7373d2d811df
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-26 14:39:19 +0100

    chore: add notification routes

--- meetby-backend/src/routes/user.js
+++ meetby-backend/src/routes/user.js
@@ -4,4 +4,10 @@ const usersController = require('../controllers/userController')
 module.exports = (app) => {
     app.route('/users')
         .get(authJwtMiddleware.verify, usersController.getUsers)
+    
+    app.route('/user/notifications')
+        .get(authJwtMiddleware.verify, usersController.getAllNotifications)
+    
+    app.route('/user/notifications/:notificationId')
+        .put(authJwtMiddleware.verify, usersController.markNotificationAsRead)
 }
