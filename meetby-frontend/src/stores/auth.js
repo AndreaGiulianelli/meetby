1db2e1621ef5fcec04a058d030aea36ffca77fd2
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-27 14:27:53 +0100

    chore: store also name surname and username

--- meetby-frontend/src/stores/auth.js
+++ meetby-frontend/src/stores/auth.js
@@ -5,6 +5,9 @@ import authService from '@/services/AuthorizationService.js'
 export const useAuthStore = defineStore('auth', () => {
     const authToken = ref(null)
     const userId = ref(null)
+    const name = ref(null)
+    const surname = ref(null)
+    const username = ref(null)
 
     const isLoggedIn = computed(() => authToken.value != null)
 
@@ -13,8 +16,14 @@ export const useAuthStore = defineStore('auth', () => {
         if (response) {
             authToken.value = response.accessToken
             userId.value = response.userId
+            name.value = response.name
+            surname.value = response.surname
+            username.value = response.username
             localStorage.setItem("authToken", authToken.value)
             localStorage.setItem("userId", userId.value)
+            localStorage.setItem("name", name.value)
+            localStorage.setItem("surname", surname.value)
+            localStorage.setItem("username", username.value)
             return true
         }
         return false
@@ -28,11 +37,17 @@ export const useAuthStore = defineStore('auth', () => {
     }
 
     function initStore() {
-        if (localStorage.getItem("authToken")) {
+        if (localStorage.getItem("authToken") 
+            && localStorage.getItem("userId")
+            && localStorage.getItem("name")
+            && localStorage.getItem("surname")
+            && localStorage.getItem("username")
+        ) {
             authToken.value = localStorage.getItem("authToken")
-        }
-        if (localStorage.getItem("userId")) {
             userId.value = localStorage.getItem("userId")
+            name.value = localStorage.getItem("name")
+            surname.value = localStorage.getItem("surname")
+            username.value = localStorage.getItem("username")
         }
     }
 
