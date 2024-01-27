6de9e6e5467116c49db6981037f1d859b7582981
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-27 14:30:13 +0100

    chore: add notification item component

---
+++ meetby-frontend/src/components/notification/NotificationItem.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+    notificationId: String,
+    meetId: String,
+    html: String,
+    date: Date,
+    read: Boolean,
+})
+
+const emit = defineEmits(['notification-click'])
+
+function notificationClicked() {
+    emit('notification-click')
+}
+</script>
+
+<template>
+    <v-list-item
+        @click="notificationClicked"
+        :value="notificationId"
+        class="px-0 rounded"
+    >
+        <template v-slot:title>
+            <p class="text-wrap" v-html="html"></p>
+        </template>
+        <template v-slot:append v-if="!read">
+            <span class="text-paletteBlue pr-2">●</span>
+        </template>
+    </v-list-item>
+    <v-divider></v-divider>
+</template>
