e59467db614ee8ce999ddc12eca00457f168645b
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-27 14:31:01 +0100

    feat: add notification icon

--- meetby-frontend/src/components/layout/AppBar.vue
+++ meetby-frontend/src/components/layout/AppBar.vue
@@ -3,9 +3,11 @@ import { ref } from 'vue'
 import { useDisplay } from 'vuetify'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth.js'
+import { useNotificationStore } from '@/stores/notification.js'
 
 const drawer = ref(false)
-const store = useAuthStore()
+const authStore = useAuthStore()
+const notificationStore = useNotificationStore()
 const router = useRouter()
 const { mobile } = useDisplay()
 
@@ -23,7 +25,7 @@ function switchDrawer() {
 }
 
 function logout() {
-    store.logout()
+    authStore.logout()
     router.push({ name: "home" })
 }
 
@@ -32,14 +34,21 @@ function logout() {
 <template>
     <v-app-bar :elevation="0">
         <v-app-bar-nav-icon icon="mdi-menu" aria-label="Menu" size="x-large" @click.stop="switchDrawer"/>
-            <v-app-bar-title class="app-bar font-weight-black" @click.stop="router.push('/')" :aria-label="store.isLoggedIn ? 'My meets' : 'Home'">
+            <v-app-bar-title class="app-bar font-weight-black" @click.stop="router.push('/')" :aria-label="authStore.isLoggedIn ? 'My meets' : 'Home'">
                 meetby
             </v-app-bar-title>
         <v-spacer />
+        
+        <v-btn v-if="authStore.isLoggedIn" bg-color="paletteWhite" aria-label="Notifications" size="large" to="/notifications">
+            <v-badge v-if="notificationStore.unreadNotificationCounter > 0" :content="notificationStore.unreadNotificationCounter">
+                <v-icon size="large" icon="mdi-bell-outline"/>
+            </v-badge>
+            <v-icon v-else size="large" icon="mdi-bell-outline"/>
+        </v-btn>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" floating :class="{ 'w-100': mobile }">
-        <v-list density="compact" nav v-if="store.isLoggedIn">
+        <v-list density="compact" nav v-if="authStore.isLoggedIn">
             <v-list-item 
                 v-for="item in items"
                 :prepend-icon="item.icon"
@@ -53,7 +62,7 @@ function logout() {
         
         <template v-slot:append>
             <div class="pa-2 mb-4">
-                <v-btn block class="black-btn" @click.prevent="logout" v-if="store.isLoggedIn">
+                <v-btn block class="black-btn" @click.prevent="logout" v-if="authStore.isLoggedIn">
                     Logout
                 </v-btn>
                 <template v-else>
