f979bee64208d705e304c4378a725c58390fe347
Andrea Giulianelli <andrygiulio@gmail.com>
2024-01-28 17:15:28 +0100

    fix: avoid to parse message already parsed

--- meetby-backend/src/routes/socketRoutes.js
+++ meetby-backend/src/routes/socketRoutes.js
@@ -2,6 +2,6 @@ const chatController = require('../controllers/chatController')
 
 module.exports = (io, socket) => {
     socket.on('message:new', (message) => {
-        chatController.sendNewMessage(io, socket, JSON.parse(message))
+        chatController.sendNewMessage(io, socket, message)
     })
 }
