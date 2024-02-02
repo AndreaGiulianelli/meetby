821bac1bdd89172a8f727e94016b5e39da637640
Andrea Giulianelli <andrygiulio@gmail.com>
2024-02-02 12:19:33 +0100

    chore: confirm dialog for deletion in entry

--- meetby-frontend/src/components/ui/DeletableEntry.vue
+++ meetby-frontend/src/components/ui/DeletableEntry.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { ref } from 'vue'
+
 defineProps({
     text: String,
 })
 const emit = defineEmits(['deleted'])
 
+const deleteDialog = ref(false)
+
 function deleted() {
     emit('deleted')
+    deleteDialog.value = false
 }
 </script>
 
@@ -20,13 +25,38 @@ function deleted() {
         </template>
 
         <template v-slot:append>
-            <v-icon 
-                aria-label="Delete"
-                aria-hidden="false"
-                icon="mdi-close"
-                color="paletteBlack"
-                @click.prevent="deleted"
-            ></v-icon>
+            <v-dialog width="auto" persistent v-model="deleteDialog">
+                <template v-slot:activator="{ props }">
+                    <v-icon 
+                        aria-label="Delete"
+                        aria-hidden="false"
+                        icon="mdi-close"
+                        color="paletteBlack"
+                        v-bind="props"
+                    ></v-icon>
+                </template>
+                <v-card class="py-5">
+                        <v-card-title class="font-weight-bold">
+                            Delete the entry?
+                        </v-card-title>
+                        <v-card-text class="pt-0 px-4">Do you really want to delete the entry '<span class="font-weight-bold">{{ text }}</span>'?</v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="paletteBlack"
+                                variant="text"
+                                aria-label="No"
+                                @click="deleteDialog = false"
+                            >No</v-btn>
+                            <v-btn
+                                color="paletteRed"
+                                variant="text"
+                                aria-label="Yes"
+                                @click="deleted"
+                            >Yes</v-btn>
+                        </v-card-actions>
+                    </v-card>
+            </v-dialog>
         </template>
     </v-card>
 </template>
